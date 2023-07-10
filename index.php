3408a0f4e52e8940c8610ecda3bcf96cb62d3d3e
S-Lombardi <silo.lombardi@gmail.com>
2023-07-10 22:25:58 +0200

    Modifiche stile header e card

--- index.php
+++ index.php
@@ -40,125 +40,126 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Boolshop</title>
-</head>
-<body>
-    <!-- HEADER -->
-    <header class="text-center mb-5">
-        <h1>Boolshop</h1>
-    </header>
-    <!-- FINE HEADER -->
-    <!-- CARD Prodotti -->
-    <div class="container">
-        <div class="row">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <title>Boolshop</title>
+    </head>
+    <body>
+        <!-- HEADER -->
+        <header class="bg-danger-subtle text-center mb-5">
+            <h1 class="text-dark p-2">
+                Boolshop
+            </h1>
+        </header>
+        <!-- FINE HEADER -->
+        <!-- CARD Prodotti -->
+        <div class="container">
+            <div class="row">
+                <!--PRODOTTI CIBO -->
+                <?php foreach($array_cibo as $cibo){ ?>
+                    <div class="col-4 mb-3">
+                        <div class="card h-100">
 
-            <!--PRODOTTI CIBO -->
-            <?php foreach($array_cibo as $cibo){ ?>
-                <div class="col-4">
-                    <div class="card mb-3">
-
-                        <!-- Immagine prodotto -->
-                        <img src=<?php  echo $cibo->getImg(); ?> alt="" class=" ">
-                        
-                        <!-- Info prodotto-->
-                        <div class="card-body ">
-                            <h4>
-                                <?php  echo $cibo->getNome() ?>
-                            </h4>
-                            <p>
-                                <i class="fa-solid <?php echo $cibo->icona ?>" ></i>
-                                <?php  echo $cibo->getAnimale() ?>
-                            </p>
-                            <p>
-                                Prezzo: &euro; <?php  echo  $cibo->getPrezzo() ?>
-                            </p>
-                            <p>
-                                Peso neto: <?php  echo $cibo->getPeso() ?>g
-                            </p>
-                            <p>
-                                Ingredinti: <?php  echo $cibo->getIngredienti() ?>
-                            </p>
+                            <!-- Immagine prodotto -->
+                            <img src=<?php  echo $cibo->getImg(); ?> alt="" class=" ">
+                            
+                            <!-- Info prodotto-->
+                            <div class="card-body ">
+                                <h4>
+                                    <?php  echo $cibo->getNome() ?>
+                                </h4>
+                                <p>
+                                    <i class="fa-solid <?php echo $cibo->icona ?>" ></i>
+                                    <?php  echo $cibo->getAnimale() ?>
+                                </p>
+                                <p>
+                                    Prezzo: &euro; <?php  echo  $cibo->getPrezzo() ?>
+                                </p>
+                                <p>
+                                    Peso netto: <?php  echo $cibo->getPeso() ?>g
+                                </p>
+                                <p>
+                                    Ingredienti: <?php  echo $cibo->getIngredienti() ?>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php } ?>
-            <!--FINE PRODOTTI  CIBO -->
+                <?php } ?>
+                <!--FINE PRODOTTI  CIBO -->
 
-            <!-- PRODOTTI  UTILITA -->
-            <?php foreach($array_utilita as $utilita){ ?>
-                <div class="col-4">
-                    <div class="card mb-3">
+                <!-- PRODOTTI  UTILITA -->
+                <?php foreach($array_utilita as $utilita){ ?>
+                    <div class="col-4 mb-3">
+                        <div class="card h-100">
 
-                        <!-- Immagine prodotto -->
-                        <img src=<?php  echo $utilita->getImg(); ?> alt="" class=" ">
-                        
-                        <!-- Info prodotto-->
-                        <div class="card-body ">
-                            <h4>
-                                <?php  echo $utilita->getNome() ?>
-                            </h4>
-                            <p>
-                                <i class="fa-solid <?php echo $utilita->icona ?>" ></i>
-                                <?php  echo $utilita->getAnimale() ?>
-                            </p>
-                            <p>
-                                Prezzo: &euro; <?php  echo  $utilita->getPrezzo() ?>
-                            </p>
-                            <p>
-                                Peso neto: <?php  echo $utilita->getMateriale() ?>
-                            </p>
-                            <p>
-                                Ingredinti: <?php  echo $utilita->getDimensioni() ?>
-                            </p>
+                            <!-- Immagine prodotto -->
+                            <img src=<?php  echo $utilita->getImg(); ?> alt="" class=" ">
+                            
+                            <!-- Info prodotto-->
+                            <div class="card-body ">
+                                <h4>
+                                    <?php  echo $utilita->getNome() ?>
+                                </h4>
+                                <p>
+                                    <i class="fa-solid <?php echo $utilita->icona ?>" ></i>
+                                    <?php  echo $utilita->getAnimale() ?>
+                                </p>
+                                <p>
+                                    Prezzo: &euro; <?php  echo  $utilita->getPrezzo() ?>
+                                </p>
+                                <p>
+                                    Peso neto: <?php  echo $utilita->getMateriale() ?>
+                                </p>
+                                <p>
+                                    Ingredienti: <?php  echo $utilita->getDimensioni() ?>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php } ?>
-            <!--FINE PRODOTTI UTILITA -->
+                <?php } ?>
+                <!--FINE PRODOTTI UTILITA -->
 
-            <!--FINE PRODOTTI GIOCO -->
-            <?php foreach($array_gioco as $gioco){ ?>
-                <div class="col-4">
-                    <div class="card mb-3">
+                <!--FINE PRODOTTI GIOCO -->
+                <?php foreach($array_gioco as $gioco){ ?>
+                    <div class="col-4  mb-5">
+                        <div class="card h-100">
 
-                        <!-- Immagine prodotto -->
-                        <img src=<?php  echo $gioco->getImg(); ?> alt="" class=" ">
-                        
-                        <!-- Info prodotto-->
-                        <div class="card-body ">
-                            <h4>
-                                <?php  echo $gioco->getNome() ?>
-                            </h4>
-                            <p>
-                                <i class="fa-solid <?php echo $gioco->icona ?>" ></i>
-                                <?php  echo $gioco->getAnimale() ?>
-                            </p>
-                            <p>
-                                Prezzo: &euro; <?php  echo  $gioco->getPrezzo() ?>
-                            </p>
-                            <p>
-                                Peso neto: <?php  echo $gioco->getCaratteristiche() ?>
-                            </p>
-                            <p>
-                                Ingredinti: <?php  echo $gioco->getDimensioni() ?>
-                            </p>
+                            <!-- Immagine prodotto -->
+                            <img src=<?php  echo $gioco->getImg(); ?> alt="" class=" ">
+                            
+                            <!-- Info prodotto-->
+                            <div class="card-body ">
+                                <h4>
+                                    <?php  echo $gioco->getNome() ?>
+                                </h4>
+                                <p>
+                                    <i class="fa-solid <?php echo $gioco->icona ?>" ></i>
+                                    <?php  echo $gioco->getAnimale() ?>
+                                </p>
+                                <p>
+                                    Prezzo: &euro; <?php  echo  $gioco->getPrezzo() ?>
+                                </p>
+                                <p>
+                                    Peso neto: <?php  echo $gioco->getCaratteristiche() ?>
+                                </p>
+                                <p>
+                                    Ingredienti: <?php  echo $gioco->getDimensioni() ?>
+                                </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            <?php } ?>
+                <?php } ?>
 
-            <!--FINE PRODOTTI GIOCO -->
+                <!--FINE PRODOTTI GIOCO -->
 
+            </div>
         </div>
-    </div>
 
-    <!-- FINE CARD Prodotti -->
+        <!-- FINE CARD Prodotti -->
 
 
-</body>
+    </body>
 </html>
